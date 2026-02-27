741bc8ef11411f86b3b38fab65d1d1344f6ed210
Shaheen Akram <shaheenakengineer@gmail.com>
2026-02-27 19:31:22 +0500

    Create quality_check_gold.sql for data validation
    Add SQL queries for quality checks on gold data.

---
+++ tests/quality_check_gold.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT gender FROM gold.dim_customers
+SELECT * FROM gold.dim_products
+SELECT * FROM gold.fact_sales
+-- foreign key integrity 
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL
